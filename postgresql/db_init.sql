67b06a2bb960ae33683b9e5583178863414c06bf
ragingroland <ragingroland@yandex.ru>
2026-06-18 08:19:30 +0300

    работа над dwh_init.sql

--- postgresql/db_init.sql
+++ postgresql/db_init.sql
@@ -1,11 +1,27 @@
 create database db_vdep;
+
 create user lydemere with password 'easy12345';
-grant connect on database db_vdep to lydemere;
+grant all on database db_vdep to lydemere;
+
 \c db_vdep;
+
 create schema if not exists s_vdep;
+
 grant all on schema s_vdep to lydemere;
+
 set search_path to s_vdep, public;
-create table if not exists s_vdep.t_usertable (id uuid, insertion_date timestamp, entered_user varchar);
+
+create table if not exists s_vdep.t_usertable (
+    id bigint generated always as identity (start with 1 increment by 1 no maxvalue) primary key,
+    user_uuid uuid not null,
+    insertion_date timestamp not null,
+    entered_user varchar not null);
+create index if not exists idx_t_usertable on s_vdep.t_usertable(user_uuid);
+
+alter table s_vdep.t_usertable owner to lydemere;
+
 grant all on all tables in schema s_vdep to lydemere;
 alter default privileges in schema s_vdep grant all on tables to lydemere;
-insert into s_vdep.t_usertable select gen_random_uuid(), clock_timestamp(), current_user;
+
+insert into s_vdep.t_usertable (user_uuid, insertion_date, entered_user) select gen_random_uuid(), clock_timestamp(), current_user;
+analyze s_vdep.t_usertable;
